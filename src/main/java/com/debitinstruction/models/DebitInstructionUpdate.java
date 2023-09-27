74eae96c0ab04e362596cf65a14c100a79d04920
Elliot Wilson <elliotwilson96@gmail.com>
2023-09-27 19:09:42 +0100

    Added validation

--- src/main/java/com/debitinstruction/models/DebitInstructionUpdate.java
+++ src/main/java/com/debitinstruction/models/DebitInstructionUpdate.java
@@ -1,9 +1,7 @@
 package com.debitinstruction.models;
 
-
 import io.micronaut.core.annotation.Introspected;
 import io.micronaut.serde.annotation.Serdeable;
-
 import java.time.LocalDate;
 
 @Introspected
@@ -17,23 +15,19 @@ public class DebitInstructionUpdate {
     public int getDebInstructSelectedDay() {
         return debInstructSelectedDay;
     }
+
     public void setDebInstructSelectedDay(int debInstructSelectedDay) {
+        if (debInstructSelectedDay < 1 || debInstructSelectedDay > 31) {
+            throw new IllegalArgumentException("The day value must be between 1 and 31.");
+        }
         this.debInstructSelectedDay = debInstructSelectedDay;
     }
 
     public LocalDate getDebInstructNextPaymentDate() {
         return debInstructNextPaymentDate;
     }
+
     public void setDebInstructNextPaymentDate(LocalDate debInstructNextPaymentDate) {
         this.debInstructNextPaymentDate = debInstructNextPaymentDate;
     }
-
-    @Override
-    public String toString() {
-        return "DebitInstructionUpdate{" +
-                "debInstructSelectedDay=" + debInstructSelectedDay +
-                ", debInstructNextPaymentDate=" + debInstructNextPaymentDate +
-                '}';
-    }
 }
-
